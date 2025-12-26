2f04c9110146f58c56021ec10a21faa774b6de1f
Elliu12 <elliu.bustamante@estudiante.laamericanuniversity.edu.ni>
2025-12-26 14:17:38 -0600

    probando django

---
+++ django_templates/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'home.html',{})
+
+def logout(request, name):
+    context = {'name' : name}
+    return render(request,'logout.html',context)
+
+def login(request, nombre, cargo):
+
+    tecnologias = ['Python','Django','Flask','PhP','Laravel']
+    context = {
+        'Nombre' : nombre,
+        'Cargo' : cargo,
+        'tecnologias' : tecnologias
+    }
+
+    return render(request,'login.html',context)
